905c400918b6cd36c91a0b0114619a83925fd7e6
Sam Rooney <the.sam.rooney@gmail.com>
2024-12-07 16:21:25 -0500

    started work on day6 part2, but moving to a different computer

--- solutions/day6.js
+++ solutions/day6.js
@@ -34,8 +34,37 @@ function day6part1(input) {
 }
 
 function day6part2(input) {
+    let input_lines = input.split("\n");
+    let original_map = [];
+    for (let row of input_lines) {
+        original_map.push(Array.from(row));
+    }
+    let position = find_starting_point(input_lines);
+    let rows = input_lines.length;
+    let cols = input_lines[0].length;
+    let direction = get_next_direction(undefined);
+    let traveled_positions = []
+    while (true) {
+        let next_position = Array.from(position);
+        if (original_map[position[0][position[1]]] != "X") {
+            traveled_positions.push(position);
+        }
+        original_map[position[0]] = replaceCharInString(original_map[position[0]], position[1], "X");
+        next_position[0] += direction[0];
+        next_position[1] += direction[1];
+        if (next_position[0] >= rows || next_position[0] < 0 || next_position[1] >= cols || next_position[1] < 0) {
+            break;
+        } else if (original_map[next_position[0]][next_position[1]] != "#") {
+            position = next_position;
+        } else {
+            direction = get_next_direction(direction);
+        }
+    }
+
     return undefined;
 }
+
+function detect_loop_in_map = 
 
 function find_starting_point(input_lines) {
     for (let row in input_lines) {
