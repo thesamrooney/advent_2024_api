1733f5fce589ea90059c22bb95fe35cc9e8c8b18
Sam Rooney <the.sam.rooney@gmail.com>
2024-12-07 03:34:20 -0500

    add template day

---
+++ solutions/template.js
@@ -0,0 +1,17 @@
+
+export function solvedayX(input) {
+    let result = {
+        "part1": dayXpart1(input),
+        "part2": dayXpart2(input)
+    }
+    return result;
+}
+
+function dayXpart1(input) {
+    return undefined;
+}
+
+function dayXpart2(input) {
+    return undefined;
+}
+
