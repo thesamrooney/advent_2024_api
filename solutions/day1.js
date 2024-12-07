7fbf07bc8a4f83e3f5467c0ce2ce60a6b75ca3b9
Sam Rooney <the.sam.rooney@gmail.com>
2024-12-07 03:33:04 -0500

    update days 1 and 2 placeholder - haven't copied my solutions over yet

--- solutions/day1.js
+++ solutions/day1.js
@@ -1,5 +1,17 @@
 
 export function solveday1(input) {
-    let result = `Day 1 not implemented yet :(`;
+    let result = {
+        "part1": day1part1(input),
+        "part2": day1part2(input)
+    }
     return result;
 }
+
+function day1part1(input) {
+    return undefined;
+}
+
+function day1part2(input) {
+    return undefined;
+}
+
--- solutions/day2.js
+++ solutions/day2.js
@@ -1,5 +1,17 @@
 
 export function solveday2(input) {
-    let result = `Day 2 not implemented yet :(`;
+    let result = {
+        "part1": day2part1(input),
+        "part2": day2part2(input)
+    }
     return result;
 }
+
+function day2part1(input) {
+    return undefined;
+}
+
+function day2part2(input) {
+    return undefined;
+}
+
