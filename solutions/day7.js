58f08971b93d1378c20f58555fa47537d74fb50f
Sam Rooney <sam@rooney.sh>
2024-12-07 18:03:07 -0500

    complete day 7 part 2 (day 7 done!)

--- solutions/day7.js
+++ solutions/day7.js
@@ -31,7 +31,26 @@ function day7part1(input) {
 }
 
 function day7part2(input) {
-    return undefined;
+    let lines = parseInput(input);
+    let valid_sum = 0;
+    let valid_counter = 0;
+    for (let line of lines) {
+        let target_result = parseInt(line[0]);
+        let numbers = line[1];
+        let possibilities = [numbers[0]];
+        for (let i = 1; i < numbers.length; i++) {
+            possibilities = generateNextPossibilities2(possibilities, numbers[i]);
+        }
+        for (let possibility of possibilities) {
+            if (calculateEqn(possibility) == target_result) {
+                valid_sum += target_result;
+                valid_counter++;
+                break;
+            }
+        }
+    }
+    console.log(`found ${valid_counter} valid eqns out of ${lines.length}`)
+    return valid_sum;
 }
 
 function parseInput(input) {
@@ -58,6 +77,8 @@ function calculateEqn(equation) {
             value += parseInt(equation[i+1]);
         } else if (equation[i] == "*") {
             value *= parseInt(equation[i+1]);
+        } else if (equation[i] == "||") {
+            value = parseInt(String(value) + String(equation[i+1]));
         } else {
             console.warn(`invalid equation ${equation} detected at index ${i} - ${equation[i]}`);
         }
@@ -90,3 +111,33 @@ function generateNextPossibilities(possibilities_so_far, next_number) {
     return new_possibilities;
 }
 
+function generateNextPossibilities2(possibilities_so_far, next_number) {
+    // possibility looks like [n, +, n, *, n, ...] (all strings)
+    let new_possibilities = [];
+    if (possibilities_so_far.length == 1) {
+        let new_possibility_plus = [possibilities_so_far[0], "+", next_number];
+        let new_possibility_times = [possibilities_so_far[0], "*", next_number];
+        let new_possibility_concat = [possibilities_so_far[0], "||", next_number];
+        new_possibilities.push(new_possibility_plus);
+        new_possibilities.push(new_possibility_times);
+        new_possibilities.push(new_possibility_concat);
+        return new_possibilities;
+    }
+    for (let possibility of possibilities_so_far) {
+        let new_possibility_plus = Array.from(possibility)
+        new_possibility_plus.push("+");
+        new_possibility_plus.push(next_number);
+        new_possibilities.push(new_possibility_plus);
+        
+        let new_possibility_times = Array.from(possibility)
+        new_possibility_times.push("*");
+        new_possibility_times.push(next_number);
+        new_possibilities.push(new_possibility_times);
+
+        let new_possibility_concat = Array.from(possibility)
+        new_possibility_concat.push("||");
+        new_possibility_concat.push(next_number);
+        new_possibilities.push(new_possibility_concat);
+    }
+    return new_possibilities;
+}
